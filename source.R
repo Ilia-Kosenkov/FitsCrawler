da93ff7205df35e08ab24692ecdc765ad23b33f1
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-10-25 01:13:05 +0300

    Wrote custom algorithm

--- source.R
+++ source.R
@@ -1,22 +1,49 @@
 box::use(
   FITSio[readFITS, readFITSheader, parseHdr],
   fs[fs_path = path],
+  stringr[str_sub, str_trim, str_locate_all],
   tibble[as_tibble],
-  tidyr[pivot_wider],
-  dplyr[mutate, n]
+  dplyr[mutate, across, if_else],
+  purrr[map_if, map2_int]
 )
 
 get_keys <- function(path) {
   path |> 
-    readFITSheader() |>
-    parseHdr() |>
+    readFITSheader() -> input
+  
+  keys <- input |>
+    str_sub(1, 8) |>
+    str_trim("right")
+  
+  vals <- input |>
+    str_sub(10) |>
     as_tibble() |>
     mutate(
-      Id = (1L:n() + 1L) %/% 2L,
-      Type = rep(c("Key", "Value"), n() / 2L)
+      Quote = str_locate_all(value, "(?<=')"),
+      Slash = str_locate_all(value, "/"),
+      across(
+        c(Quote, Slash),
+        \(x) map_if(x, \(.x) isTRUE(length(.x) > 0), max, .else = \(.x) -1L)
+      )
     ) |>
-    pivot_wider(Id, names_from = Type, values_from = value) 
+    mutate(
+      Last = map2_int(Quote, Slash, max),
+      .keep = "unused"
+    ) |>
+    mutate(
+      value = 
+        if_else(
+          Last == -1L, 
+          value, 
+          str_sub(value, 1L, Last - 1L)
+        ) |>
+        str_trim(),
+      .keep = "unused"
+    ) |>
+    mutate(key = keys, .before = value)
 }
+
+
 
 fs_path("G:", "Data", "B", "20190722", "HD204827", "HD204827_B_0001.fits") |> 
   get_keys() |>
