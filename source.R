6c3212cb470f32413733681de170cd928039b3bc
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-10-25 11:42:26 +0300

    Test processing data

--- source.R
+++ source.R
@@ -4,8 +4,13 @@ box::use(
   keys_info = ./keys_info[...],
   file_data = ./file_data[...],
   dplyr[mutate, slice],
-  purrr[map_dfr = map_dfr]
+  # purrr[map_dfr = map_dfr],
+  furrr[map_dfr = future_map_dfr],
+  future[ft_plan = plan, ft_cluster = cluster],
+  tictoc[tic, toc]
 )
+
+ft_plan(ft_cluster(workers = 6L))
 
 proc_file <- function(path) {
   path |>
@@ -14,9 +19,12 @@ proc_file <- function(path) {
 }
 
 
+tic()
 
-fs_path("G:", "Data", "B", "20210706", "MAXIJ1820") |>
+fs_path("G:", "Data", "B", "20210706") |>
   dir_ls(glob = "*.fits", recurse = TRUE) |>
   file_data$get() |>
   mutate(map_dfr(path, proc_file)) |>
-  print(n = 10000)
+  print()
+
+toc()
